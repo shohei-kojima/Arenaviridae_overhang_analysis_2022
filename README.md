768e677935796bc1c2d6e68da4a876b867ee7b09
shohei-kojima <26kojima@gmail.com>
2022-01-16 19:39:18 +0900

    20220116-193918

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Arenaviridae_overhang_analysis_2022
+# Analyze overhangs of Arenaviruses  
+  
+### Download Arenaviruses in NCBI refseq  
+The sequences of arenaviruses in NCBI refseq database was downloaded by the following command on 2022 Jan 07. Then, the overhangs in the downloaded sequences were analyzed by a custom Python script.  
+```
+# download sequences
+esearch -db nucleotide -query "Viruses[Organism] AND srcdb_refseq[PROP] NOT cellular organisms[Organism] AND Arenaviridae[Organism] OR Arenaviridae[All Fields]" | efetch -format fasta > Arenaviridae_210107_nuclotide.fa
+
+# analyze presence of overhangs
+python batch_220107_233000.py
+```
+  
+  
+### Software versions  
+- Python 3.7.4  
+
+
---
+++ batch_220107_233000.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+"""
+2022/01/07 23:30:00 +0900
+
+# batch_220107_233000.py
+# generated at /home/kooojiii/results/2021/misc/matsumoto_san/210107_1
+# usage: python %prog
+# python3.7
+
+"""
+
+import os,sys,glob,gzip
+import string
+
+
+start='CGCA'
+start_pos_threshold=8
+length_threshold=1000
+len_start=len(start)
+
+
+def parse_fasta(path_to_file):
+    tmp={}
+    seq=[]
+    with open(path_to_file) as infile:
+        for line in infile:
+            if '>' in line and seq:
+                tmp[header]=''.join(seq)
+                header=line.strip().replace('>', '')
+                seq=[]
+            elif '>' in line and not seq:
+                header=line.strip().replace('>', '')
+            else:
+                seq.append(line.strip())
+        tmp[header]=''.join(seq)
+    return tmp
+
+def complement(seq):
+    return seq.translate(str.maketrans('ATGCatgc', 'TACGtacg'))[::-1]
+
+def format_overhang(seq):
+    for pos in range(len(seq) - len_start + 1):
+        if seq[pos:pos + len_start] == start:
+            overhang=seq[:pos]
+            break
+    return ['%s|%s' % (overhang, seq[pos:]), '%s|%s' % (overhang, seq[pos:pos + len_start]), overhang]
+
+
+f='Arenaviridae_210107_nuclotide.fa'
+fa=parse_fasta(f)
+out=['refseq\tfasta_header\tseq(5...3)\t5end\t3end(complement)\t5overhang\t3overhang(complement)\t5overhang_length\t3overhang_length\n']
+for h in fa:
+    seq=fa[h].upper().replace('U', 'T')
+    if len(seq) < length_threshold:
+        continue
+    cseq=complement(seq)
+    if start in seq[:start_pos_threshold] and start in cseq[:start_pos_threshold]:
+        l_format_full,l_format_trimmed,l_overhang=format_overhang( seq[:start_pos_threshold])
+        r_format_full,r_format_trimmed,r_overhang=format_overhang(cseq[:start_pos_threshold])
+        r_format_full=complement(r_format_full)
+        out.append('%s\t%s\t%s...%s\t%s\t%s\t%s\t%s\t%d\t%d\n' % (h.split(' ')[0], h, l_format_full, r_format_full, l_format_trimmed, r_format_trimmed, l_overhang, r_overhang, len(l_overhang), len(r_overhang)))
+
+print(len(out))
+with open('arenaviridae_overhangs.tsv', 'w') as outfile:
+    outfile.write(''.join(out))
